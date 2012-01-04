38a6e9b94677281da0efd9cca4b3b8cf2af7cdfd
Sergey Kostenko <stcrasher@gmail.com>
2012-01-04 18:14:22 +0000

    trying to add "remove" link for categories

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,5 +1,5 @@
 class Category < ActiveRecord::Base
   has_many :products
   acts_as_nested_set
-  accepts_nested_attributes_for :children, :reject_if => lambda { |a| a[:name].blank? }
+  accepts_nested_attributes_for :children, :reject_if => lambda { |a| a[:name].blank? }, :allow_destroy => true
 end
