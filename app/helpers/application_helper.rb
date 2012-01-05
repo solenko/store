a93b12b036a348006506bd83b255d1f800a8d8d3
Sergey Kostenko <stcrasher@gmail.com>
2012-01-05 17:21:19 +0000

    fix "add" link for categories

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,12 +4,13 @@ module ApplicationHelper
   end
 
 def link_to_add_fields(name, f, association)
-  new_object = f.class.reflect_on_association(association).klass.new
-  fields = fields_for(association, new_object, :child_index => "new_#{association}") do |child|
-    render(association.to_s.singularize + "_fields", :child => child)
+    new_object = f.object.class.reflect_on_association(association).klass.new
+
+    fields = f.fields_for(association, new_object, :child_index => "new_#{association}") do |builder|
+      render(:partial => association.to_s.singularize + "_fields", :locals => { :f => builder})
+    end
+    link_to_function(name, "add_fields(this, '#{association}', '#{escape_javascript(fields)}')")
   end
-  link_to_function(name, "add_fields(this, \"#{association}\", \"#{escape_javascript(fields)}\")")
-end
 
 
 end
