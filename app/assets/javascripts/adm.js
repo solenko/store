9936e8284c4c469cfffdf27bbefd4bc6afda23d2
Sergey <stcrasher@gmail.com>
2012-01-26 23:25:07 +0000

    slider's style changes

--- app/assets/javascripts/adm.js
+++ app/assets/javascripts/adm.js
@@ -1,15 +1,3 @@
-//jQuery(function($){
-//    // Bind handler to click event
-//    $("a.remove-fields").click(function(e){
-//        var $this = $(this);
-//        $this.prev("input[type=hidden]").val("1");
-//        $this.closest(".fields").hide();
-//        // Prevent default behavior
-//        e.preventDefault();
-//    });
-//});
-//var remove_fields;
-
 jQuery(function($){
   remove_fields = function(link) {
     $(link).prev("input[type=hidden]").val("1");
