bbc80035e54ab2e22ed4a3743ce7cfd184630a81
Sergey <stcrasher@gmail.com>
2012-02-02 00:18:41 +0000

    add preview for cached images in admin

--- app/models/product.rb
+++ app/models/product.rb
@@ -16,7 +16,7 @@
 
   accepts_nested_attributes_for :productsizes, :allow_destroy => true
   accepts_nested_attributes_for :productcategories, :reject_if => lambda { |a| a[:category_id].blank?}, :allow_destroy => true
-  accepts_nested_attributes_for :productimages, :reject_if => lambda { |a| a[:image].blank?}, :allow_destroy => true
+  accepts_nested_attributes_for :productimages, :reject_if => lambda { |a| a[:image].blank? and a[:image_cache].blank? }, :allow_destroy => true
 
   validates :art, :presence => true, :uniqueness => true
   validates :name, :presence => true, :uniqueness => true
@@ -24,6 +24,7 @@
   validates_presence_of :productimages, :message => "не могут быть пустыми"
   validates_presence_of :productcategories, :message => "не могут быть пустыми"
     
+  
   
   def category_without_id
     productcategories.each do |pc|
