46b2b6e83a30ab6b1b3e4b7dd13522fa0c6caf04
Sergey Kostenko <stcrasher@gmail.com>
2012-02-07 18:18:51 +0000

    fix endless pagination

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -2,8 +2,8 @@ jQuery(function($) {
   if ($('.pagination .page').length) {
     $(window).scroll(function() {
       var url;
-      url = $('.pagination .next_page').attr('href');
-      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 250) {
+      url = $('.pagination .next_page a').attr('href');
+      if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
         $('.pagination').text("Fetching more products...");
         return $.getScript(url);
       }
