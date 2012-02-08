81eaee8fd75806917529fc4df255e0f0e28de34a
Sergey Kostenko <stcrasher@gmail.com>
2012-02-08 12:07:15 +0000

    del AR session store

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -28,6 +28,7 @@ Store::Application.configure do
   # Specifies the header that your server uses for sending files
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
   config.action_dispatch.x_sendfile_header = 'X-Accel-Redirect' # for nginx
+  #config.static_cache_control = "public, max-age=3600"
 
   # Force all access to the app over SSL, use Strict-Transport-Security, and use secure cookies.
   # config.force_ssl = true
--- db/schema.rb
+++ db/schema.rb
@@ -55,16 +55,6 @@ ActiveRecord::Schema.define(:version => 20120204125505) do
     t.datetime "updated_at"
   end
 
-  create_table "sessions", :force => true do |t|
-    t.string   "session_id", :null => false
-    t.text     "data"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-  end
-
-  add_index "sessions", ["session_id"], :name => "index_sessions_on_session_id"
-  add_index "sessions", ["updated_at"], :name => "index_sessions_on_updated_at"
-
   create_table "sizes", :force => true do |t|
     t.string   "code"
     t.datetime "created_at"
