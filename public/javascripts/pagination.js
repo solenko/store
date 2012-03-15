c7156d936ceaeb2fb36922e54661818f5f021abb
Sergey Kostenko <stcrasher@gmail.com>
2012-03-15 13:43:12 +0000

    minor fixes for endless pagination

--- public/javascripts/pagination.js
+++ public/javascripts/pagination.js
@@ -4,7 +4,7 @@ jQuery(function($) {
       var url;
       url = $('.pagination .next_page a').attr('href');
       if (url && $(window).scrollTop() > $(document).height() - $(window).height() - 50) {
-        $('.pagination').text("Fetching more products...");
+        $('.pagination').text("Загрузка товаров...");
         return $.getScript(url);
       }
     });
