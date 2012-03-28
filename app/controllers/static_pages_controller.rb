cdda5ecfe97aee18acffa1d4420f7f7e6c99effd
Sergey Kostenko <stcrasher@gmail.com>
2012-03-28 17:47:10 +0000

    example for ckeditor and static_pages

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -6,7 +6,23 @@
   end
 
   def contacts
+    @text = StaticPage.find_by_name("contacts")
     @title = "Контакты"
+  end
+  
+  def stock
+    @text = StaticPage.find_by_name("stock")
+	@title = "Акции"
+  end
+  
+  def terms
+    @text = StaticPage.find_by_name("terms")
+	@title = "Условия"
+  end
+  
+  def about
+    @text = StaticPage.find_by_name("about")
+	@title = "О Нас"
   end
 
 end
