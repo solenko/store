3035454e0344e4213e6a7abcf02b14510f0b05ed
Sergey Kostenko <stcrasher@gmail.com>
2012-03-28 17:52:04 +0000

    add new routes for static pages

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,25 @@ Store::Application.routes.draw do
 
   devise_for :users, :controllers => { :sessions => "admin/sessions" }
 
+  #static begin
   get "main" => "static_pages#main"
+  get "stock" => "static_pages#stock"
+  get "about" => "static_pages#about"
+  get "terms" => "static_pages#terms"
   get "contacts" => "static_pages#contacts"
-  get "admin" => "admin/admin#index"
-  match '/admin/products/build_image/:id', :controller=>'admin/products', :action=>'build_image'
+  #static end
   
+  match '/admin/products/build_image/:id', :controller=>'admin/products', :action=>'build_image'
+
+  #catalog begin
   match 'newest', :to => 'products#newest', :defaults => {:mode => 'newest'}, :as => 'newest_products'
   match 'newest/:id', :to => 'products#show', :defaults => {:mode => 'newest'}, :as => 'newest_product'
   match 'products(/:category_id)', :to => 'products#index', :defaults => {:mode => 'all' }, :as => 'products'
   match 'product(/:category_id)/:id', :to => 'products#show', :defaults => {:mode => 'all' }, :as => 'product'
+  #catalog end
 
+  # admin begin
+  get "admin" => "admin/admin#index"
   namespace :admin do
     resources :users
     resources :products
@@ -19,7 +28,8 @@ Store::Application.routes.draw do
     resources :sizes
     resources :static_pages
   end
- 
+  #admin end
+
   root :to => 'static_pages#main'
 
 end
